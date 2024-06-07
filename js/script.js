72d9c46b3459f1fe010d45d2b2d262b686f15912
unknown <negi13.p@gmail.com>
2024-06-08 00:08:07 +0530

    files added

--- js/script.js
+++ js/script.js
@@ -1,6 +1,16 @@
 
-let closeBtn = document.querySelector('.close-btn');
-let sidebar = document.querySelector('.sidebar');
-closeBtn.addEventListener('click', () => {
-  sidebar.classList.toggle('active');
+let closeBtn = document.querySelector('.sidebar .close-btn');
+closeBtn.addEventListener('click', (event) => {
+  event.target.closest('.sidebar').classList.toggle('active');
 })
+
+let selectBtn = document.querySelector('.select');
+let periodPoUp = document.querySelector('.period-po-up');
+selectBtn.addEventListener('click', () => {
+  periodPoUp.classList.toggle('active');
+})
+
+let closeBtnP = document.querySelector('.period-po-up .close-btn');
+closeBtnP.addEventListener('click', (event) => {
+  event.target.closest('.period-po-up').classList.remove('active');
+});
